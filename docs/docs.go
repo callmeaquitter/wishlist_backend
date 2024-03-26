4ceb31c7cbc7c8fc0501620ae9269f49e7918eeb
DoomGuy1818 <zasdqs745@gmail.com>
2024-03-27 02:18:54 +0300

    +big fix

--- docs/docs.go
+++ docs/docs.go
@@ -4536,7 +4536,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:7777",
+	Host:             "80.249.145.126:80",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Wishlist",
